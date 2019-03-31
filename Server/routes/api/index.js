40a78c8441e6f6c39df81347ca3eae42616201fa
sampad789 <sampad789@gmail.com>
2019-03-31 15:24:17 +0300

    server changes too

--- Server/routes/api/index.js
+++ Server/routes/api/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const newsRoute = require('./news');
-const matchesRoute = require('./match');
-const resultsRoute = require('./results')
+const newsRoute = require("./news");
+const matchesRoute = require("./match");
+const resultsRoute = require("./results");
+const rankingsRoute = require("./rankings");
+const teamsRoute = require("./teams");
 
 /* Same URL as in people.js */
-router.use('/news', newsRoute);
-router.use ('/match',matchesRoute)
-router.use('/results',resultsRoute)
+router.use("/news", newsRoute);
+router.use("/match", matchesRoute);
+router.use("/results", resultsRoute);
+router.use("/rankings", rankingsRoute);
+router.use("/teams", teamsRoute);
 
 module.exports = router;
