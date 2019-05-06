72462d30f167dae077070d572c9cd11be77d2935
sampad789 <sampad789@gmail.com>
2019-05-06 11:03:19 +0300

    v1.1 deployed

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.get("/", function(req, res) {
   res.send("Working fine !!!");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
